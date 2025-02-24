cd95431daee4e1aa14d3b94e5a976c7dc3f753e6
maruf.islam <maruf.islam@miaki.co>
2025-02-24 12:35:08 +0600

    fix: webhook ready

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,46 +1,19 @@
 const express = require('express')
 
 const router = express.Router();
-
 router.get('/', (req, res) => {
   res.send({ message: 'Welcome' });
 });
 
-router.post('/ipn', (req, res) => {
+router.use('*', (req, res) => {
+  console.log({body: req.body})
   res.send({
     response: {
-      data: req.body,
-      message: "Payment IPN send successfully"
+      data: req.body,     
+      message: "Redirect Data"
     }
   })
 })
 
-router.post('/status/success', (req, res) => {
-  res.send({
-    response: {
-      data: req.body,
-      message: 'Payment success'
-    }
-  })
-})
-
-router.post('/status/cancel', (req, res) => {
-  console.log(req.body)
-  res.send({
-    response: {
-      data: req.body,
-      message: 'Payment cancel'
-    }
-  })
-})
-
-router.post('/status/fail', (req, res) => {
-  res.send({
-    response: {
-      data: req.body,
-      message: 'Payment failure'
-    }
-  })
-})
 
 module.exports = router;
